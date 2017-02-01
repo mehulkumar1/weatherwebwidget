67201f932c752205c9a34e08614d47668cfda12c
mehulkumar1 <mehulkumar.100@gmail.com>
2017-02-01 17:19:08 +0530

    created static weather app using angular js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
     $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22fairfax%2C%20va%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithke').
         then(function(response) {
-            $scope.greeting = response.data;
+            $scope.weather = response.data;
         });
 });
